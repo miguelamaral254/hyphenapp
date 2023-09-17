9108837dd6dbd3096b03aedb48750f6fd736c95b
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2023-09-17 12:48:06 -0300

    att

--- hyphen-app/screens/HomeScreen.js
+++ hyphen-app/screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, Text, SafeAreaView, TouchableOpacity, Image} from "react-native";
 import tw from "tailwind-react-native-classnames";
+import useAuth from "../hooks/useAuth"
 const HomeScreen = () => {
+  const {user , logout} = useAuth()
   return (
     <SafeAreaView style={tw.style("flex-1 mt-6")}>
       <View>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={logout}>
           <Image
             style={tw.style("h-10 w-10 rounded-full")}
             source={{
--- hyphen-app/screens/LoginScreen.js
+++ hyphen-app/screens/LoginScreen.js
@@ -1,69 +1,87 @@
-import { View, Text, ImageBackground, TextInput, TouchableOpacity, Alert } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import tw from "tailwind-react-native-classnames"
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from '@firebase/auth'
-import { auth } from '../firebase'
-import useAuth from "../hooks/useAuth"
+import { View, Text, TextInput, TouchableOpacity, Alert, Image } from "react-native";
+import React, { useLayoutEffect, useState } from "react";
+import tw from "tailwind-react-native-classnames";
+import {
+  updateProfile,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../firebase";
+import useAuth from "../hooks/useAuth";
+import { useNavigation } from "@react-navigation/native";
+import { ImageBackground } from "react-native";
 
 const LoginScreen = () => {
-  const [type, setType] = useState(2) //1. signIn or 2.signUp
-  const {loading, setLoading} = useAuth()
+  const [type, setType] = useState(1); //1.signin 2.signup
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  useEffect(()=> {
-    setName("");
-    setEmail("");
-    setPassword("");
-  },[type]);
+  const { loading, setLoading } = useAuth();
+
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
   const signIn = () => {
     if (email.trim() === "" || password.trim === "") {
       return Alert.alert("Ohhh!!", "You have not entered all details");
     }
-    setLoading(true)
+    setLoading(true);
 
     signInWithEmailAndPassword(auth, email, password)
-      .then(({user})=>{
-        setLoading(false)
+      .then((userCredential) => {
+        setLoading(false);
       })
-      .catch((err) => {
-        setLoading(false)
-        
+      .catch((error) => {
+        setLoading(false);
       });
   };
+
   const signUp = () => {
     if (name.trim() === "" || email.trim() === "" || password.trim === "") {
       return Alert.alert("Ohhh!!", "You have not entered all details");
     }
-    createUserWithEmailAndPassword(auth, email, password).then(({user})=>{
-      updateProfile(user, {displayName:name});
-      console.log(user)
+    setLoading(true);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(({ user }) => {
+        updateProfile(user, { displayName: name });
+        setLoading(false);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        setLoading(false);
+      });
+  };
 
-    })
-    .catch((err)=> {
-      console.log(err);
-    })
-    
-  }
-  if(loading) {
+  if (loading) {
     return (
       <View style={tw.style("flex-1 justify-center items-center")}>
-        <Text style={tw.style("font-semibold text-red-400 text-2xl")}>Loading...</Text>
+        <Text style={tw.style("font-semibold text-red-400 text-2xl")}>
+          Loading....
+        </Text>
       </View>
-    )
+    );
   }
+
   return (
     <ImageBackground
       style={tw.style("flex-1 bg-black")}
       resizeMode="cover"
+      source={require("../assets/navista.jpg")}
       
     >
-      
       {type === 1 ? (
         <View style={tw.style("flex-1 justify-center items-center")}>
-          <Text style={tw.style("font-bold text-2xl text-green-700")}>Sign In</Text>
+          
+          <Text style={tw.style("font-bold text-2xl")}>Sign In</Text>
           <Text style={tw.style("text-white font-semibold")}>
             Access to your account
           </Text>
@@ -90,7 +108,7 @@ const LoginScreen = () => {
               onChangeText={(text) => setPassword(text)}
             />
             <TouchableOpacity
-              style={tw.style("w-full rounded-lg mt-8 bg-green-600 py-3")}
+              style={tw.style("w-full rounded-lg mt-8 bg-green-400 py-3")}
               onPress={signIn}
             >
               <Text style={tw.style("text-center text-white font-bold")}>
@@ -104,27 +122,34 @@ const LoginScreen = () => {
             </TouchableOpacity>
           </View>
         </View>
-      ) : 
-      // sign up
-      (
+      ) : (
         <View style={tw.style("flex-1 justify-center items-center")}>
           <Text style={tw.style("font-bold text-2xl")}>Sign Up</Text>
           <Text style={tw.style("text-white")}>Create a new account</Text>
           <View style={tw.style("w-full p-5")}>
-            <Text style={tw.style("font-semibold pb-2 text-gray-300")}>Name</Text>
+            <Text style={tw.style("font-semibold pb-2 text-white")}>Name</Text>
             <TextInput
-            style={tw.style("bg-black border border-gray-300 text-sm text-gray-300 rounded-lg w-full p-2.5 mb-4")}
-            value={name}
-            onChangeText={(text)=> setName(text)}/>
+              keyboardType="default"
+              style={tw.style(
+                "bg-black border border-gray-300 text-sm text-gray-300 rounded-lg w-full p-2.5 mb-4"
+              )}
+              value={name}
+              onChangeText={(text) => setName(text)}
+            />
             <Text style={tw.style("font-semibold pb-2 text-white")}>Email</Text>
             <TextInput
-            keyboardType="email-address" 
-            style={tw.style("bg-black border border-gray-300 text-sm text-gray-300 rounded-lg w-full p-2.5 mb-4")}
-            value={email}
-            onChangeText={(text) => setEmail(text)}/>
-            <Text style={tw.style("font-semibold pb-2 text-white")}>Password</Text>
+              keyboardType="email-address"
+              style={tw.style(
+                "bg-black border border-gray-300 text-sm text-gray-300 rounded-lg w-full p-2.5 mb-4"
+              )}
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              secureTextEntry={false}
+            />
+            <Text style={tw.style("font-semibold pb-2 text-white")}>
+              Password
+            </Text>
             <TextInput
-              
               secureTextEntry={true}
               style={tw.style(
                 "bg-black border border-gray-300 text-sm text-gray-300 rounded-lg w-full p-2.5 mb-4"
@@ -136,18 +161,20 @@ const LoginScreen = () => {
               style={tw.style("w-full rounded-lg mt-8 bg-black py-3")}
               onPress={signUp}
             >
-              <Text style={tw.style("text-center text-white font-bold")}>Sign Up</Text>
+              <Text style={tw.style("text-center text-white font-bold")}>
+                Sign Up
+              </Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={()=>setType(1)}> 
-              <Text style={tw.style("text-center text-gray-100 pt-3")}>Already have an account?</Text>
+            <TouchableOpacity onPress={() => setType(1)}>
+              <Text style={tw.style("text-center text-gray-100 pt-3")}>
+                Already have an account?
+              </Text>
             </TouchableOpacity>
           </View>
         </View>
-      )
-    }  
-
+      )}
     </ImageBackground>
-  )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
