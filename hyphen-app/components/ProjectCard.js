e70d3e9a5c054b6299aa0d642d1e7583d93f1852
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2023-10-03 09:35:21 -0300

    Create ProjectCard.js

---
+++ hyphen-app/components/ProjectCard.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ProjectCard = () => {
+  return (
+    <View>
+      <Text>ProjectCard</Text>
+    </View>
+  );
+};
+
+export default ProjectCard;
